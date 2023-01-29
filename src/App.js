0d014ea19bea4ec90624ed6c50977051187f7b80
Norbert Csipak <norbertcipak@me.com>
2023-01-29 16:16:22 +0000

    practicing passing props, state of components,..

--- src/App.js
+++ src/App.js
@@ -1,38 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
 
 // using props to pass a value from a function to render in the app
 // props are initialised in App where th ecomponent is rendered
-function Greetings(props) {
-  return(
-    <div>
-      <p>Hello {props.name}</p>
-    </div>
-  )
-}
+// function Greetings(props) {
+//   return(
+//     <div>
+//       <p>Hello {props.name}</p>
+//     </div>
+//   )
+// }
 
-function Footer() {
+// function Footer() {
+//   return(
+//     <div>
+//       Greeting app created by <a href='https://github.com/norbertcsipak'>norbertcsipak</a>
+//     </div>
+//   )
+// }
+
+function Header(props) {
   return(
     <div>
-      Greeting app created by <a href='https://github.com/norbertcsipak'>norbertcsipak</a>
+      <h1>{props.course}</h1>
     </div>
   )
 }
 
 function App() {
-  const now = new Date();
-  const a = 10;
-  const b = 20;
-  console.log(now, a+b);
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercise1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercise2 = 7;
+  const part3 = 'State of a component';
+  const exercise3 = 14;
 
   return (
     <div className="App">
-      <h1>Hello world, it is {now.toString()}</h1>
+      {/* <h1>Hello world, it is {now.toString()}</h1>
       <p>
         {a} + {b} is {a + b}
       </p>
       <Greetings name="Alex" />
-      <Footer />
+      <Footer /> */}
+
+      <Header course={course}/>
+      <p>
+        {part1} {exercise1}
+      </p>
+      <p>
+        {part2} {exercise2}
+      </p>
+      <p>
+        {part3} {exercise3}
+      </p>
+      <p>
+        Number of exercises {exercise1 + exercise2 + exercise3}
+      </p>
     </div>
   );
 }
