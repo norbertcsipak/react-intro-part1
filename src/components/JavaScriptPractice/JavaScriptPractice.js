28130c510d74ed467d7512be09cd835854e6e175
Norbert Csipak <norbertcipak@me.com>
2023-02-01 10:06:02 +0000

    arrays

--- src/components/JavaScriptPractice/JavaScriptPractice.js
+++ src/components/JavaScriptPractice/JavaScriptPractice.js
@@ -1,34 +1,48 @@
 import React from 'react';
 
 export default function JavaScriptPractice () {
-  function PrintingInConsole () {
-    const x = 1;
-    let y = 5;
+  // function PrintingInConsole () {
+  //   const x = 1;
+  //   let y = 5;
 
-    // JS will print in console the initialised values for x and y
-    console.log(x, y);
+  //   // JS will print in console the initialised values for x and y
+  //   console.log(x, y);
 
-    // After incrementing y, the console prints out the new value of y
-    y += 10;
-    console.log(x, y);
+  //   // After incrementing y, the console prints out the new value of y
+  //   y += 10;
+  //   console.log(x, y);
 
-    // If assigning string to y, the console will not throw an error. Instead
-    // It will print the value of x and the new string value of y
-    y = 'text';
-    console.log(x, y);
+  //   // If assigning string to y, the console will not throw an error. Instead
+  //   // It will print the value of x and the new string value of y
+  //   y = 'text';
+  //   console.log(x, y);
 
-    // In JS you cannot re-assign a new value of varuable declared with const
-    // This will throw an error in the console
-    x += 4;
-    console.log(x, y);
+  //   // In JS you cannot re-assign a new value of varuable declared with const
+  //   // This will throw an error in the console
+  //   // x += 4;
+  //   // console.log(x, y);
+  // }
+
+  function ArraysInJs () {
+    const instruments = ['Guitar', 'Piano', 'Violine', 'Trumpet'];
+
+    instruments.push('Flute');
+    console.log(instruments.length);
+    console.log(instruments[2]);
+
+    return(
+      <div>
+        {instruments.forEach((instrument) => {
+          console.log(instrument);
+        })}
+      </div>
+    )
   }
-
-
 
   return(
     <div>
       <h1>This is JS Practice page...</h1>
-
+      <ArraysInJs />
     </div>
   )
 }
